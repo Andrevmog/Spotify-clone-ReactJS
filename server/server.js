899773a89d8c0614b9f83fc35f19002f05ce7ba4
andrevmog <andre.machado@zeeng.com.br>
2021-10-13 19:02:59 -0300

    inicio

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const app = express();
+
+
+app.post('/login', (req,res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId : '62d6cd8dba1b449380eedadf191feb39',
+        clientSecret: '05ced4cf5a15486d946ac9a71aa4aea2'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(() => {
+        res.sendStatus(400)
+    })
+})
---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+
+var client_id = '62d6cd8dba1b449380eedadf191feb39'; // Your client id
+var client_secret = 'CLIENT_SECRET'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+
+
+
+export default function Login(){
+    return (
+    
+    <Container
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh"}}>
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>Login with Spotify</a>
+    </Container>
+
+    )
+}
+
